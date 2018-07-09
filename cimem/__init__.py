FIX: Fix a bug in a computation of the cost
The cost function of the MEM currently assumes that the Bayesian
network is fully connected. In most cases, this will not be the
true.

This commit modifies the cost function to use the normalization
coefficient of the whole network rather than of one independent
branch.

cimem/__init__.py
@@ -52,8 +52,9 @@ def solve(data: np.ndarray, clusters: Sequence[Cluster]) \
 
         marginals.update()
 
+        # Compute the MEM cost.
         entropy = -np.dot(lagrange, data)
-        entropy += np.log(marginals[clusters[0]].normalization)
+        entropy += np.log(marginals.normalization)
 
         weights = [p for c in clusters for p in marginals[c].probabilities]
         gradient = -data